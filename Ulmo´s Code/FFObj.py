03df9e6cf20e74c79735f1cae3ae601366d00d74
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2018-07-16 13:00:24 -0700

    new
    commit

--- Ulmo´s Code/FFObj.py
+++ Ulmo´s Code/FFObj.py
@@ -1,3 +1,4 @@
+import os
 import CiclesReader
 from CiclesReader import *
 
@@ -35,13 +36,6 @@ class FF:
 
 
 #MAIN 
-
-
-a = cicleReader("Data\Cycles\FF-5_amplicon1_cycles.txt")
-f = FF(a[0],a[1],a[2])
-
-
-	
 #geneList
 
 #for i in geneList:
@@ -50,42 +44,44 @@ f = FF(a[0],a[1],a[2])
 #print (turner)
 #print("------------")
 #print(a[2])
-aux = set()
-aux2 = set()
-writeStuff= ""
-countBools = 0
-for nc in f.cycles:
-	#print(f.cycles[nc][1:2])
-	for i in geneList:
-		#print(i[1:2][0])
-		if f.cycles[nc][1:2]!= "" and f.cycles[nc][2:3]!= "" and i[1:2][0]!= "" and i[2:3][0]!= "":
-			if Intersegments(int(f.cycles[nc][1:2][0]),int(f.cycles[nc][2:3][0]),int(i[1:2][0]),int(i[2:3][0])):
-				b = a[2][nc][3]
-				#print(b)
-				#print("Cycle info " + str(a[2][nc]) + " geneName" + str(i[4:5]) )
-				#print("Entre al ultimo if")ssssss
-				#c = "Sample "+"FF-4" + "; Cycle info " + str(a[2][nc]) + "; geneName" + str(i[4:5])
-				for x in turner:
-					for j in turner[x]:
-						#print("Entre a los 2 for")
-						if len(j)>1:
-							aux.add(j)
-					if b:
-						countBools += 1
-						#print(countBools)
-						aux2.add(str(i[4:5]) + ",")
-					#print (aux)
-					if countBools>= 1:
-						writeStuff+=("Sample "+"FF-4" + "\t Turner info: "+ str(aux) + "\t All genes:" + str(i[4:5])+ "\t Cycle Genes:" + str(aux2) + "\t Has a cycle -> YES \n\n" )
-					else:
-						writeStuff+=("Sample "+"FF-4" + "\t Turner info: "+ str(aux) + "\t All genes:" + str(i[4:5])+ "\t Cycle Genes:" + str(aux2) + "\t Has a cycle -> NO\n\n" )
-					aux2.clear()
-					aux.clear()
-				#continue
-		#aux = ""
 
 
-with open("prueba.txt","w") as file2:
-	file2.write(writeStuff)
-	#file2.write(str(geneList))
+def writer(f,a):
+	writeStuff= ""
+	aux = set()
+	aux2 = set()
+	countBools = 0
+	for nc in f.cycles:
+		#print(f.cycles[nc][1:2])
+		for i in geneList:
+			#print(i[1:2][0])
+			if f.cycles[nc][1:2]!= "" and f.cycles[nc][2:3]!= "" and i[1:2][0]!= "" and i[2:3][0]!= "":
+				if Intersegments(int(f.cycles[nc][1:2][0]),int(f.cycles[nc][2:3][0]),int(i[1:2][0]),int(i[2:3][0])):
+					b = a[2][nc][3]
+					#print(b)
+					#print("Cycle info " + str(a[2][nc]) + " geneName" + str(i[4:5]) )
+					#print("Entre al ultimo if")ssssss
+					#c = "Sample "+"FF-4" + "; Cycle info " + str(a[2][nc]) + "; geneName" + str(i[4:5])
+					for x in turner:
+						for j in turner[x]:
+							#print("Entre a los 2 for")
+							if len(j)>1:
+								aux.add(j)
+						if b:
+							countBools += 1
+							#print(countBools)
+							aux2.add(str(i[4:5]) + ",")
+						#print (aux)
+							if countBools>= 1:
+								writeStuff+=("Sample "+"FF-4" + "\t Turner info: "+ str(aux) + "\t All genes:" + str(i[4:5])+ "\t Cycle Genes:" + str(aux2) + "\t Has a cycle -> YES \n\n" )
+							else:
+								writeStuff+=("Sample "+"FF-4" + "\t Turner info: "+ str(aux) + "\t All genes:" + str(i[4:5])+ "\t Cycle Genes:" + str(aux2) + "\t Has a cycle -> NO\n\n" )
+							aux2.clear()
+							aux.clear()
+	with open("prueba.txt","a") as file2:
+		file2.write(writeStuff)
 
+for file in os.listdir(os.getcwd()+"\Data\Cycles"):
+	a = cicleReader(file)
+	f = FF(a[0],a[1],a[2])
+	writer(f,a)
